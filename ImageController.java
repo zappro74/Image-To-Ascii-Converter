5736ddad836f0cb2b9467da592beeed3b0c50222
zappro74 <150842322+zappro74@users.noreply.github.com>
2025-10-07 13:23:43 -0500

    add variables and started convertPixels()

--- ImageController.java
+++ ImageController.java
@@ -2,9 +2,19 @@ class ImageController
 {
     private static int[][] originalPixels = ImageLoader.getPixels();
     private static int[][] newPixels;
+    private static int originalHeight = ImageLoader.getHeight();
+    private static int originalWidth = ImageLoader.getWidth();
+    private static int userWidth = UserInput.getUserWidth();
     
-    public static int[][] convertPixels()
+    public static void convertPixels()
     {
-        
+        for (int y = 0; y < originalHeight; y++)
+        {
+            for (int x = 0; x < originalWidth; x++)
+            {
+                
+            }
+        }
+
     }
 }
