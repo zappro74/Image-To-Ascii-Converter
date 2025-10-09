3a96860290d8a1482cd8e710422dfa6455292d49
zappro74 <150842322+zappro74@users.noreply.github.com>
2025-10-08 20:21:25 -0500

    init variables

---
+++ Convolution.java
@@ -0,0 +1,10 @@
+class Convolution
+{
+    private static int[][] originalPixels = ImageLoader.getPixels();
+    private static int originalHeight = ImageLoader.getHeight();
+    private static int originalWidth = ImageLoader.getWidth();
+    private static int originalSize = originalHeight * originalWidth;
+    private static int userWidth = UserInput.getTargetWidth();
+
+    
+}
