3e1566617f3a7b31197c93f975be6dcaa9139ac3
zappro74 <150842322+zappro74@users.noreply.github.com>
2025-10-13 15:09:12 -0500

    init run()

--- Program.java
+++ Program.java
@@ -1,4 +1,26 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
 class Program
 {
+    UserInput user = new UserInput();
     
+    public void run() 
+    {
+        try 
+        {
+            File file = user.requestFile();
+            BufferedImage image = ImageLoader.loadImage(file);
+            double[][] brightness = ImageLoader.calculatePixels(image);
+
+            System.out.println("Image processed successfully!");
+            System.out.println("Pre-configured dimensions: " + image.getWidth() + "x" + image.getHeight());
+        } 
+        catch (FileNotFoundException e) 
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
