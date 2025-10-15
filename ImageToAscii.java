9a52206ff625fd047ab97435deedf19c596a6f70
zappro74 <150842322+zappro74@users.noreply.github.com>
2025-10-15 11:38:22 -0500

    toAsciiLines() complete

--- ImageToAscii.java
+++ ImageToAscii.java
@@ -4,9 +4,25 @@ class ImageToAscii
 {
     private static final String DEFAULT_RAMP = "@%#*+=-:.";
 
-    public static void toAsciiLines(double[][] brightness)
+    public static ArrayList<String> toAsciiLines(double[][] brightness)
     {
         ArrayList<String> lines = new ArrayList<>();
-        
+        String ramp = DEFAULT_RAMP;
+
+        for (int y = 0; y < brightness.length; y++)
+        {
+            String row = "";
+            for (int x = 0; x < brightness[0].length; x++)
+            {
+                double value = brightness[y][x];
+                int index = (int) ((value / 225.0) * (ramp.length() - 1));
+
+                row += ramp.charAt(index);
+            }
+            
+            lines.add(row);
+        }
+
+        return lines;
     }
 }
