02290adf6c54a46ed0cb9f5e7f6570ee339a2a0e
zappro74 <150842322+zappro74@users.noreply.github.com>
2025-10-15 13:20:54 -0500

    optimize output (only printed half of the image before lol)

--- ImageController.java
+++ ImageController.java
@@ -6,21 +6,22 @@ class ImageController
     {
         int originalWidth = image.getWidth();
         int originalHeight = image.getHeight();
-        int targetHeight = (int) (targetWidth * ((double) originalHeight / originalWidth));
+        int targetHeight = (int) Math.max(1 , Math.round(targetWidth * ((double) originalHeight / originalWidth)));
         double[][] resizedPixels = new double[targetHeight][targetWidth];
+
+        double scaleX = (double) originalWidth / targetWidth;
+        double scaleY = (double) originalHeight / targetHeight;
 
         for (int y = 0; y < targetHeight; y++)
         {
+            int sourceY = Math.min((int) Math.floor(y * scaleY), originalHeight - 1);
             for (int x = 0; x < targetWidth; x++)
             {
-                int sourceX = (int) (x * ((double) originalWidth / targetWidth) * .5);
-                int sourceY = (int) (y * ((double) originalHeight / targetHeight) * .5);
+                int sourceX = Math.min((int) Math.floor(x * scaleX), originalWidth - 1);
                 resizedPixels[y][x] = originalPixels[sourceY][sourceX];
             }
         }
-
         System.out.println("Image (Converted) dimensions: " + targetWidth + "x" + targetHeight);
-
         return resizedPixels;
     }
 }
