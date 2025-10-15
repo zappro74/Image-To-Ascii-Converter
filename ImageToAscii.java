2eaddce39e14d5616ce90517d86203a5db0a671d
zappro74 <150842322+zappro74@users.noreply.github.com>
2025-10-15 11:08:11 -0500

    init toAsciiLines()

--- ImageToAscii.java
+++ ImageToAscii.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+
 class ImageToAscii
 {
-    
+    private static final String CHAR_RAMP = "@%#*+=-:.";
+
+    public static ArrayList<String> toAsciiLines(double[][] brightness)
+    {
+
+    }
 }
